a3896a55742ed0d1a13bb6357c32c684ec74e95a
Zoni Faay <zonifaay@gmail.com>
2025-12-09 12:03:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const count = document.querySelector('.count');
+const resetBtn = document.querySelector('.reset-btn');
 const minusBtn = document.querySelector('.minus-btn');
 const plusBtn = document.querySelector('.plus-btn');
 const statusText = document.querySelector('.status');  
-const resetBtn = document.querySelector('.reset-btn');
+ 
 
-// Function to update even/odd text
+minusBtn.addEventListener('click', () => {
+    const countValue = parseInt(count.innerText);
+    count.innerText = countValue - 1;
+    updateStatus(); 
+});
+
+
+plusBtn.addEventListener('click', () => {
+    const countValue = parseInt(count.innerText);
+    count.innerText = countValue + 1;
+    updateStatus(); 
+});
+
+resetBtn.addEventListener('click', () => {
+    count.innerText = 0;
+    updateStatus();
+});
+
 function updateStatus() {
     const value = parseInt(count.innerText);
     if (value % 2 === 0) {
@@ -16,25 +34,7 @@ function updateStatus() {
     }
 }
 
-// Minus button
-minusBtn.addEventListener('click', () => {
-    const countValue = parseInt(count.innerText);
-    count.innerText = countValue - 1;
-    updateStatus(); // <-- update text
-});
-
-// Plus button
-plusBtn.addEventListener('click', () => {
-    const countValue = parseInt(count.innerText);
-    count.innerText = countValue + 1;
-    updateStatus(); // <-- update text
-});
-
-resetBtn.addEventListener('click', () => {
-    count.innerText = 0;
-    updateStatus(); 
-});
-
  
 
  
+
